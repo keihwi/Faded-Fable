2a3c77dbf339ddc569f26129092da164de8fc370
austinb03 <148805439+austinb03@users.noreply.github.com>
2025-10-07 19:42:35 -0400

    fixed dash and next for dialogue

--- scripts/mc.gd
+++ scripts/mc.gd
@@ -1,58 +1,74 @@
 extends CharacterBody2D
 
-
 var speed = 130.0
 const JUMP_VELOCITY = -330.0
+const DASH_VELOCITY = 120.0
+const DASH_DURATION = 0.3  
 
 @onready var dash_timer: Timer = $DashTimer
 @onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
+@onready var can_dash_timer: Timer = $canDashTimer
+
 var can_dash = true
+var is_dashing = false
+# Current DASH mechanic allows the player to almost float for a few seconds if you press space right before dash, keep in maybe?
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
+	# Add gravity (only if not dashing)
+	if not is_on_floor() and not is_dashing:
 		velocity += get_gravity() * delta
 
-	# Handle jump.
-	if Input.is_action_just_pressed("jump") and is_on_floor():
+	# Handle jump (disabled while dashing)
+	if Input.is_action_just_pressed("jump") and is_on_floor() and not is_dashing:
 		velocity.y = JUMP_VELOCITY
-	
-	if Input.is_action_just_pressed("sprint") and can_dash:
-		dash_timer.start()
-		speed = 430.0
-		can_dash = false
-		
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
+
 	var direction := Input.get_axis("move_left", "move_right")
-	
-	# Flip the sprite
-	if direction > 0:
-		animated_sprite.flip_h = false
-	elif direction < 0:
-		animated_sprite.flip_h = true
-	
-	# Play animations
-	if is_on_floor():
-		if direction == 0:
-			animated_sprite.play("idle")
-		elif(!can_dash):
-			animated_sprite.play("dash")
+
+	# Handle dash
+	if Input.is_action_just_pressed("sprint") and can_dash:
+		is_dashing = true
+		can_dash = false
+		dash_timer.start(DASH_DURATION)
+
+		# Launch in facing direction
+		if animated_sprite.flip_h:
+			velocity.x = -DASH_VELOCITY/DASH_DURATION
 		else:
-			animated_sprite.play("run") 
-	elif(!can_dash):
+			velocity.x = DASH_VELOCITY/DASH_DURATION
+
 		animated_sprite.play("dash")
-	else:
-		animated_sprite.play("jump")
-	
-	if direction:
-		velocity.x = direction * speed
-	else:
-		velocity.x = move_toward(velocity.x, 0, speed)
+
+	# Normal movement
+	elif not is_dashing:
+		if direction:
+			velocity.x = direction * speed
+		else:
+			velocity.x = move_toward(velocity.x, 0, speed)
+
+	# Flip the sprite
+	if not is_dashing:
+		if direction > 0:
+			animated_sprite.flip_h = false
+		elif direction < 0:
+			animated_sprite.flip_h = true
+
+	# Handle the animations
+	if not is_dashing:
+		if is_on_floor():
+			if direction == 0:
+				animated_sprite.play("idle")
+			else:
+				animated_sprite.play("run")
+		else:
+			animated_sprite.play("jump")
 
 	move_and_slide()
 
 
 func _on_dash_timer_timeout() -> void:
+	is_dashing = false
+	can_dash_timer.start()
+
+
+func _on_can_dash_timer_timeout() -> void:
 	can_dash = true
-	speed = 130.0
