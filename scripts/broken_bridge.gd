43b3652431fb76c92e774e90aae663c7ef529a25
austinb03 <148805439+austinb03@users.noreply.github.com>
2025-10-28 18:15:01 -0400

    bridge works

--- scripts/broken_bridge.gd
+++ scripts/broken_bridge.gd
@@ -1,17 +1,15 @@
-extends AnimatableBody2D
+extends StaticBody2D
 
 @onready var honeydew: StaticBody2D = $"../NPCs/Honeydew/StaticBody2D"
-@onready var detect: CollisionShape2D = $Sense/CollisionShape2D
+@onready var step_on: CollisionShape2D = $StepOn
 
 func _ready():
 	visible = false
-	$StepOn.disabled = true
+	step_on.disabled = true
 
-
-
-func _on_sense_area_entered(area: Area2D) -> void:
-	print("player entered")
-	if(honeydew.level1_letters < 2): 
-		$StepOn.disabled = false
+func check_activation():
+	if honeydew.level1_letters >= 2:
+		print("Bridge activated!")
 		visible = true
-		
+		await get_tree().process_frame
+		$StepOn.disabled = false
--- scripts/letter_1.gd
+++ scripts/letter_1.gd
@@ -2,12 +2,11 @@ extends Area2D
 
 @onready var mc: CharacterBody2D = $"../../MC"
 @onready var honeydew: StaticBody2D = $"../../NPCs/Honeydew/StaticBody2D"
-
-
+@onready var bridge: StaticBody2D = $"../../BrokenBridge"
 
 func _on_body_entered(body: Node2D) -> void:
 	print("+1 letter")
 	honeydew.level1_letters += 1
 	print(honeydew.level1_letters)
+	bridge.check_activation()
 	queue_free()
-	
